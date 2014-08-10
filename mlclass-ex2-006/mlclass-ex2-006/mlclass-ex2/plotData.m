271771773449656f1217c5d8388f5e47de458694
dvdg <dav.vdg@gmail.com>
2014-08-10 18:04:31 +0200

    plotdata func set in ex 2

--- mlclass-ex2-006/mlclass-ex2-006/mlclass-ex2/plotData.m
+++ mlclass-ex2-006/mlclass-ex2-006/mlclass-ex2/plotData.m
@@ -4,7 +4,19 @@ function plotData(X, y)
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
 
 % Create New Figure
-figure; hold on;
+xplus = [];
+xminus = [];
+for R=1:size(y)
+    if (y(R))>.5
+        xplus = [xplus; X(R,:)];
+    else
+        xminus = [xminus; X(R,:)];
+    end
+end
+figure;
+hold on;
+plot(xplus(:,1),xplus(:,2), 'k+', 'LineWidth', 2);
+plot(xminus(:,1),xminus(:,2), 'ko', 'MarkerFaceColor', 'y');
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the positive and negative examples on a
